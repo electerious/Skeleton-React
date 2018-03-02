707d41c39d3c25b0cf604bafa7a32ef1df975b21
Tobias Reich <tobias.reich.ich@gmail.com>
2018-03-02 09:59:37 +0100

    No isClient check

--- src/assets/scripts/client.js
+++ src/assets/scripts/client.js
@@ -6,27 +6,22 @@ import h from './utils/h'
 import createStore from './utils/createStore'
 import enhanceState from './utils/enhanceState'
 import enhanceReducers from './utils/enhanceReducers'
-import isClient from './utils/isClient'
 import reducers from './reducers'
 import * as actions from './actions'
 
 import Main from './components/Main'
 
-if (isClient()===true) {
+const enhancedReducers = enhanceReducers(reducers)
+const store = createStore(enhancedReducers, window.__STATE__)
 
-	const enhancedReducers = enhanceReducers(reducers)
-	const store = createStore(enhancedReducers, window.__STATE__)
+const mapStateToProps = (state) => enhanceState(state)
+const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
 
-	const mapStateToProps = (state) => enhanceState(state)
-	const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
+const ConnectedMain = connect(mapStateToProps, mapDispatchToProps)(Main)
+const container = document.querySelector('#app')
 
-	const connectedMain = connect(mapStateToProps, mapDispatchToProps)(Main)
-	const container = document.querySelector('#app')
+const App = h(Provider, { store },
+	h(ConnectedMain)
+)
 
-	const App = h(Provider, { store },
-		h(connectedMain)
-	)
-
-	hydrate(App, container)
-
-}
+hydrate(App, container)
